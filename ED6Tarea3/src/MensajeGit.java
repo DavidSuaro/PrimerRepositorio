120a3fcbc9b50251fe4a609b9c47d8b41a3d5b8b
JD <JD@DESKTOP-86K6UOK>
2022-03-31 23:32:07 +0200

    Ya está puesto el mensaje.

---
+++ ED6Tarea3/src/MensajeGit.java
@@ -0,0 +1,11 @@
+
+public class MensajeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+			
+	}
+
+}
